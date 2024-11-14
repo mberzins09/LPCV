030cde202cb1c80607a2d40361382c7af3ffd264
mberzins09 <165376179+mberzins09@users.noreply.github.com>
2024-11-14 16:23:55 +0200

    Delete all, Start over

--- LatvijasPasts.Core/Models/Entity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LatvijasPasts.Core.Models
-{
-    public abstract class Entity
-    {
-        public int Id { get; set; }
-    }
-}
--- LatvijasPastsMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LatvijasPastsMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
